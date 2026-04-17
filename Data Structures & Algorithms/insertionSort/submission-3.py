6fb3d44a43e9b3ea17c10feaf4c6cabac6bb1da0
Suresh Chaganti <130092855+suresh-chaganti@users.noreply.github.com>
2026-04-17 07:40:44 -0400

    Add: insertionSort - submission-3

---
+++ Data Structures & Algorithms/insertionSort/submission-3.py
@@ -0,0 +1,25 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        pair_len = len(pairs)
+        pair_list = []
+        for i in range(pair_len):
+            print(i)
+            j = i -1
+            print(pairs[j].key)
+            
+            while j >=0 and pairs[j].key > pairs[j+1].key:
+                print('inside while')
+                pairs[j], pairs[j + 1] = pairs[j + 1], pairs[j]
+                j = j - 1
+                print(j)
+            pair_list.append(pairs[:])
+        
+        return pair_list
+
+
+        
