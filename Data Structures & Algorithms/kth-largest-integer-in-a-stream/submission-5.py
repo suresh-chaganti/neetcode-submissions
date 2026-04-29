eb212eb4c884887a4f2c3234ddd3a839b709a5f3
Suresh Chaganti <130092855+suresh-chaganti@users.noreply.github.com>
2026-04-29 04:06:17 -0400

    Add: kth-largest-integer-in-a-stream - submission-5

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-5.py
@@ -0,0 +1,25 @@
+class KthLargest:
+
+    # def __init__(self, k: int, nums: List[int]):
+    #     self.k = k 
+    #     self.nums = nums
+        
+
+    # def add(self, val: int) -> int:
+    #     self.nums.append(val)
+    #     sorted_nums = sorted(self.nums)
+    #     return sorted_nums[-self.k]
+
+        
+
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
