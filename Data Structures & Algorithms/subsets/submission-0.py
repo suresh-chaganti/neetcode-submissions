1513d5f054403f62523c4098bf9b4fbd6f2c3dda
Suresh Chaganti <130092855+suresh-chaganti@users.noreply.github.com>
2026-05-01 08:42:49 -0400

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        
+        for num in nums:
+            res += [curr + [num] for curr in res]
+        
+        return res
+
+        
